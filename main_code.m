aed75158454fca08f9391ecf92371c0c78fe2a3d
Shivam Singh <iec2014016@iiita.ac.in>
2017-02-25 07:27:50 -0800

    Delete main_code.m

--- main_code.m
+++
@@ -1,5 +0,0 @@
-folder = fullfile(matlabroot, 'real length of pen');
-baseFileName = 'pen1.jpg';
-fullFileName = fullfile(folder, baseFileName); 
-grayImage = imread(fullFileName);                       %return the details of image
-[rows columns numberOfColorBands] = size(grayImage)     %show dimension of image in pixel
